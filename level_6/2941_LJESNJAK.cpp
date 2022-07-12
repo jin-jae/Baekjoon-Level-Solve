f34078879803c5f9dca4710bb398c5afa0da4595
김진재 <jinjae.dev@gmail.com>
2022-07-13 00:43:42 +0900

    2022-07-13

--- level_6/2941_LJESNJAK.cpp
+++ level_6/2941_LJESNJAK.cpp
@@ -6,7 +6,7 @@ int	main(void) {
 	std::cin >> s;
 	int	answer = 0;
 	for (int i = 0; i < s.size(); ) {
-		if (s[i] == 'd' && i < s.size() - 2) {
+		if (s[i] == 'd' && i < s.size() - 2 && s[i + 1] != '-') {
 			if (s[i + 1] == 'z') {
 				if (s[i + 2] == '=') {
 					answer++;
@@ -19,8 +19,19 @@ int	main(void) {
 				answer++;
 				i++;
 			}
-		} else if ((s[i] == 'c' || s[i] == 'l' || s[i] == 'n' || s[i] == 's' || s[i] == 'z') && i != s.size() - 1) {
-			if (s[i] == 'c' && (s[i + 1] == '=' || s[i + 1] == '-')) {
+		} else if ((s[i] == 'c' ||s[i] == 'd' || s[i] == 'l' || s[i] == 'n' || s[i] == 's' || s[i] == 'z') && i < s.size() - 1) {
+			if (s[i] == 'c' && (s[i + 1] == '=' || s[i + 1] == '-') || s[i] == 'd' && s[i + 1] == '-'
+					|| (s[i] == 'l' || s[i] == 'n') && s[i + 1] == 'j' || (s[i] == 's' || s[i] == 'z') && s[i + 1] == '=') {
 				answer++;
 				i += 2;
-			} else if (s[i] == 'd' && 
+			} else {
+				answer++;
+				i++;
+			}
+		} else {
+			answer++;
+			i++;
+		}
+	}
+	std::cout << answer;
+}
